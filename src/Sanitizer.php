892167301105f0427458ca2fa988369f38566c73
James Linden <kodekrash@gmail.com>
2025-04-11 00:37:37 -0400

    debugging contextual validation

--- src/Sanitizer.php
+++ src/Sanitizer.php
@@ -146,10 +146,7 @@ class Sanitizer {
 
     public static function filter(array $fields, array $errors, array $optional) : array {
         foreach ($errors as $k => $v) {
-            echo $k, PHP_EOL;
-            echo $v, PHP_EOL;
-            echo PHP_EOL;
-            if (in_array($k, $optional)) {
+            if (in_array($v, $optional)) {
                 unset($fields[$v]);
                 unset($errors[$k]);
             }
