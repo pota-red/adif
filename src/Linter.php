8395ae6c26a425059cbd05e9b760665a68b9de86
James Linden <kodekrash@gmail.com>
2025-04-15 23:46:50 -0400

    debug linter

--- src/Linter.php
+++ src/Linter.php
@@ -5,6 +5,7 @@ namespace Pota\Adif;
 class Linter {
 
     public static function lint(string $text, int $mode) : array {
+        $text = trim($text);
         $basic = preg_match('/<eoh>.*<eor>/is', $text);
         if ($basic && $mode == Document::MODE_POTA) {
             $parts = preg_split('/<eor>/i', $text);
@@ -12,9 +13,11 @@ class Linter {
                 $fields = Spec::$base_fields;
                 $errors = [];
                 foreach ($parts as $i => $part) {
-                    foreach ($fields as $field) {
-                        if (!preg_match('/<' . $field . ':[0-9]*>/is', $part)) {
-                            $errors[$i][] = $field;
+                    if (!empty($part)) {
+                        foreach ($fields as $field) {
+                            if (!preg_match('/<' . $field . ':[0-9]*>/is', $part)) {
+                                $errors[$i][] = $field;
+                            }
                         }
                     }
                 }
